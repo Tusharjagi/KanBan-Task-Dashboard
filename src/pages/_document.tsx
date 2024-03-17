6b4daa521084776f9c9051628039f87d1d09833d
TusharJagi <tushar.j@fnp.com>
2024-03-17 14:51:06 +0530

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>Kanban task management web app</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="Kanban task management dashboard" />
+          <meta name="keywords" content="keywords, separated, by, commas" />
+          <meta name="author" content="Tango" />
+          <meta property="og:title" content="kanban dashboard" />
+          <meta property="og:description" content="Kanban task management dashboard" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://kanban.com" />
+          <meta property="og:image" content="https://kanban.com/og-image.jpg" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
