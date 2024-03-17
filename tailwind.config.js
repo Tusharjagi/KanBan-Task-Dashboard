e01cd8505e389aefea8827e9ec0d0e506524b99a
TusharJagi <tushar.j@fnp.com>
2024-03-17 21:55:59 +0530

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/common/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  darkMode: "class",
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/common/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gunmetal": "#20212c",
+        "alice-blue": "#f4f7fd",
+      },
+    },
   },
   plugins: [],
-}
+};
