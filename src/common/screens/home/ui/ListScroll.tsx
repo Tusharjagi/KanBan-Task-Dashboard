769811d549a9a6e0a90a4b7c4417b9e217655b0c
TusharJagi <tushar.j@fnp.com>
2024-03-18 14:33:30 +0530

    refactor

--- src/common/screens/home/ui/ListScroll.tsx
+++ src/common/screens/home/ui/ListScroll.tsx
@@ -1,62 +1,65 @@
-import { RefObject, useEffect, useState } from 'react'
-import tw from 'tailwind-styled-components'
-import {BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs'
+import { RefObject, useEffect, useState } from "react";
+import tw from "tailwind-styled-components";
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 
 interface IProps {
-  listRef: RefObject<HTMLDivElement>
+  listRef: RefObject<HTMLDivElement>;
 }
 
-const SIDEBAR_WIDTH = 256
-const TASK_ITEM_WIDTH = 280 + 20
+const SIDEBAR_WIDTH = 256;
+const TASK_ITEM_WIDTH = 280 + 20;
 
 export function ListScroll({ listRef }: IProps) {
-  const [slide, setSlide] = useState(0)
+  const [slide, setSlide] = useState(0);
 
   useEffect(() => {
-    const listNode = listRef.current
+    const listNode = listRef.current;
 
     if (!listNode) {
-      return
+      return;
     }
 
-    listNode.style.transform = `translateX(${-slide * TASK_ITEM_WIDTH}px)`
-  }, [slide])
+    listNode.style.transform = `translateX(${-slide * TASK_ITEM_WIDTH}px)`;
+  }, [slide]);
 
   const handleOnSlidePrev = () => {
     if (slide <= 0) {
-      return
+      return;
     }
 
-    setSlide((prev) => prev - 1)
-  }
+    setSlide((prev) => prev - 1);
+  };
 
   const handleOnSlideNext = () => {
-    const listNode = listRef.current
+    const listNode = listRef.current;
 
     if (
       !listNode ||
-      window.innerWidth - SIDEBAR_WIDTH >=
-        listNode?.clientWidth - (slide - 1) * TASK_ITEM_WIDTH
+      window.innerWidth - SIDEBAR_WIDTH >= listNode?.clientWidth - (slide - 1) * TASK_ITEM_WIDTH
     ) {
-      return
+      return;
     }
 
-    setSlide((prev) => prev + 1)
-  }
+    setSlide((prev) => prev + 1);
+  };
 
   return (
     <Wrapper>
-      <ActionButton onClick={handleOnSlidePrev}><BsArrowLeftShort/></ActionButton>
-      <ActionButton onClick={handleOnSlideNext}><BsArrowRightShort/></ActionButton>
+      <ActionButton onClick={handleOnSlidePrev}>
+        <BsArrowLeftShort />
+      </ActionButton>
+      <ActionButton onClick={handleOnSlideNext}>
+        <BsArrowRightShort />
+      </ActionButton>
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = tw.div`
 flex
 mb-2
 space-x-2
-`
+`;
 
 const ActionButton = tw.button`
 px-3
@@ -66,4 +69,4 @@ text-gray-800
 bg-black/20
 dark:bg-white/20
 dark:text-white
-`
+`;
