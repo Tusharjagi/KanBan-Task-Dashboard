82b1e6adb25eb38d0ddaff3479b53b459385d363
TusharJagi <tushar.j@fnp.com>
2024-03-18 14:33:49 +0530

    set heading

--- src/common/screens/home/ui/HomeWelcomeTab.tsx
+++ src/common/screens/home/ui/HomeWelcomeTab.tsx
@@ -1,17 +1,13 @@
-import { WelcomeCard } from '@/common/components/welcome-card'
-import {
-  Header,
-  LayoutContent,
-  HeaderTitleNoSSR,
-} from '@/common/components/layout'
+import { WelcomeCard } from "@/common/components/welcome-card";
+import { Header, LayoutContent, HeaderTitleNoSSR } from "@/common/components/layout";
 
 export function HomeWelcomeTab() {
   return (
     <LayoutContent className="flex items-center">
       <Header>
-        <HeaderTitleNoSSR>Task Management</HeaderTitleNoSSR>
+        <HeaderTitleNoSSR>Kanban Dashboard</HeaderTitleNoSSR>
       </Header>
       <WelcomeCard />
     </LayoutContent>
-  )
+  );
 }
