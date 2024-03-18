1d52ae4c32e593711bd6b8e63c3529f331d9afcc
TusharJagi <tushar.j@fnp.com>
2024-03-18 14:11:23 +0530

    logo

--- src/common/components/layout/ui/sidebar/Logo.tsx
+++ src/common/components/layout/ui/sidebar/Logo.tsx
@@ -1,14 +1,24 @@
-import tw from "tailwind-styled-components";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
+
+import tw from "tailwind-styled-components";
 import blackLogo from "@/common/assets/brand/black-logo.svg";
 import whiteLogo from "@/common/assets/brand/white-logo.svg";
 import { useAppSelector } from "@/common/hooks/useRedux";
+import { mainActions } from "@/common/store/slices/main";
 
 export function Logo() {
   const { theme } = useAppSelector((x) => x.themeSlice);
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const handleOnSelect = (index: number) => () => {
+    dispatch(mainActions.updateSelectedTableIndex({ tableIndex: index }));
+  };
 
   return (
-    <div className="flex justify-start items-start">
+    <div className="flex justify-start items-start cursor-pointer " onClick={handleOnSelect(0)}>
       {theme === "dark" ? (
         <LogoStyled alt="Brand logo" src={whiteLogo} />
       ) : (
