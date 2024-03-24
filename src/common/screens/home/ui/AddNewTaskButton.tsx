3902f8271c27e37500b15f052493cb4a5ff4038b
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 13:41:59 +0530

    rm console

--- src/common/screens/home/ui/AddNewTaskButton.tsx
+++ src/common/screens/home/ui/AddNewTaskButton.tsx
@@ -1,38 +1,34 @@
-import { CreateTaskModal } from '@/common/features/task'
-import { useMediaQuery } from '@/common/hooks/useMediaQuery'
-import { useToggler } from '@/common/hooks/useToggler'
-import { ITable } from '@/common/models/ITable'
-import { Button } from '@/common/ui/button'
-import { bool2string } from '@/common/utils'
-import { useEffect } from 'react'
-import { AiOutlinePlus } from 'react-icons/ai'
+import { CreateTaskModal } from "@/common/features/task";
+import { useMediaQuery } from "@/common/hooks/useMediaQuery";
+import { useToggler } from "@/common/hooks/useToggler";
+import { ITable } from "@/common/models/ITable";
+import { Button } from "@/common/ui/button";
+import { bool2string } from "@/common/utils";
+import { useEffect } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 
 interface IProps {
-  table: ITable
-  tableIndex: number
+  table: ITable;
+  tableIndex: number;
 }
 
 export function AddNewTaskButton({ table, tableIndex }: IProps) {
-  const [open, toggleOpen] = useToggler(false)
-  const isDesktop = useMediaQuery('(min-width: 768px)')
-  const [mounted, , setMounted] = useToggler(false)
+  const [open, toggleOpen] = useToggler(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [mounted, , setMounted] = useToggler(false);
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  const content: Record<'true' | 'false', JSX.Element | string> = {
-    true: 'Add new task',
+  const content: Record<"true" | "false", JSX.Element | string> = {
+    true: "Add new task",
     false: <AiOutlinePlus className="text-white" />,
-  }
-
-  console.log(open)
+  };
 
   return (
     <>
-      <Button onClick={toggleOpen}>
-        {content[bool2string(isDesktop && mounted)]}
-      </Button>
+      <Button onClick={toggleOpen}>{content[bool2string(isDesktop && mounted)]}</Button>
       {open ? (
         <CreateTaskModal
           open={open}
@@ -42,5 +38,5 @@ export function AddNewTaskButton({ table, tableIndex }: IProps) {
         />
       ) : null}
     </>
-  )
+  );
 }
