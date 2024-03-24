125f2a63afdfd29c7ec85109ef116bffc8c7b587
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 18:49:03 +0530

    type

--- src/common/ui/portal/index.tsx
+++ src/common/ui/portal/index.tsx
@@ -1,25 +1,25 @@
-import { ReactNode, useEffect, useRef, useState } from 'react'
-import { createPortal } from 'react-dom'
+import { ReactNode, useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
 
-interface IProps {
-  children?: ReactNode
-}
+type PortalType = {
+  children?: ReactNode;
+};
 
-export function Portal({ children }: IProps) {
-  const ref = useRef<HTMLDivElement | null>(null)
-  const [mounted, setMounted] = useState(false)
+export function Portal({ children }: PortalType) {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    const element = document.createElement('div')
-    document.getElementById('__next')!.append(element)
+    const element = document.createElement("div");
+    document.getElementById("__next")!.append(element);
 
-    ref.current = element
-    setMounted(true)
+    ref.current = element;
+    setMounted(true);
 
     return () => {
-      ref.current?.remove()
-    }
-  }, [])
+      ref.current?.remove();
+    };
+  }, []);
 
-  return ref.current && mounted ? createPortal(children, ref.current) : null
+  return ref.current && mounted ? createPortal(children, ref.current) : null;
 }
