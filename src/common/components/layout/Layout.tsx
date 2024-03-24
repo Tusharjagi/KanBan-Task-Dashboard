fe0ff00305473546b0f39a18a0659c3eb4127fa4
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 17:34:20 +0530

    refactor

--- src/common/components/layout/Layout.tsx
+++ src/common/components/layout/Layout.tsx
@@ -1,69 +1,67 @@
-import { ReactNode, memo, useEffect, useState } from 'react'
-import tw from 'tailwind-styled-components'
-import { Sidebar } from './ui/sidebar'
-import { LayoutProvider, useLayoutContext } from './model/LayoutProvider'
-import { useMediaQuery } from '@/common/hooks/useMediaQuery'
-import { bool2string, string2bool } from '@/common/utils'
+import { ReactNode, memo, useEffect, useState } from "react";
+import tw from "tailwind-styled-components";
 
-interface IProps {
-  children?: ReactNode
-}
+import { Sidebar } from "./ui/sidebar";
+import { LayoutProvider, useLayoutContext } from "./model/LayoutProvider";
+import { useMediaQuery } from "@/common/hooks/useMediaQuery";
+import { bool2string, string2bool } from "@/common/utils";
 
-function WrappedComponent({ children }: IProps) {
-  const [mounted, setMounted] = useState(false)
-  const { expanded } = useLayoutContext()
-  const isDesktop = useMediaQuery('(min-width: 768px)')
+type WrappedComponentType = {
+  children?: ReactNode;
+};
+
+function WrappedComponent({ children }: Readonly<WrappedComponentType>) {
+  const [mounted, setMounted] = useState(false);
+  const { expanded } = useLayoutContext();
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
   return (
     <Wrapper $expanded={bool2string(expanded && isDesktop && mounted)}>
       <Sidebar expanded={expanded} isDesktop={isDesktop} />
       <Content>{children}</Content>
     </Wrapper>
-  )
+  );
 }
 
-export function Layout(props: IProps) {
+export function Layout(props: Readonly<WrappedComponentType>) {
   return (
     <LayoutProvider>
       <WrappedComponent {...props} />
     </LayoutProvider>
-  )
+  );
 }
 
-interface IWrapperProps {
-  $expanded: 'true' | 'false'
-}
+type WrapperType = {
+  $expanded: "true" | "false";
+};
 
-const Wrapper = tw.div<IWrapperProps>`
+const Wrapper = tw.div<WrapperType>`
 flex
 flex-row
 min-h-screen
 items-stretch
-ml-[256px]
+ml-256
 pt-16
 md:pt-20
-bg-[#f4f7fd]
-dark:bg-[#20212c]
+bg-alice-blue
+dark:bg-dark-gunmetal
 transition-none
 md:transition-[margin]
-${({ $expanded }) => (string2bool($expanded) ? 'ml-[256px]' : 'ml-0')}
-`
-/*
-
-*/
+${({ $expanded }) => (string2bool($expanded) ? "ml-256" : "ml-0")}
+`;
 
 const Content = memo(tw.div`
 flex-1
 flex
 flex-col
 items-stretch
-`)
+`);
 
 export const LayoutContent = tw.main`
 flex-1
 p-6
-`
+`;
