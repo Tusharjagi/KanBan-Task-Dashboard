5463bf88a76061e98490c2fedc00abf402a952eb
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 17:39:51 +0530

    Type

--- src/common/components/layout/ui/header/HeaderTitle.tsx
+++ src/common/components/layout/ui/header/HeaderTitle.tsx
@@ -1,25 +1,25 @@
-import tw from 'tailwind-styled-components'
-import { DetailedHTMLProps, HTMLAttributes, useState } from 'react'
-import { FiChevronDown } from 'react-icons/fi'
-import { useMediaQuery } from '@/common/hooks/useMediaQuery'
-import { Menu } from './Menu'
-import dynamic from 'next/dynamic'
+import { DetailedHTMLProps, HTMLAttributes, useState } from "react";
+import tw from "tailwind-styled-components";
+import { FiChevronDown } from "react-icons/fi";
+import dynamic from "next/dynamic";
 
-interface IProps
-  extends Omit<
-    DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>,
-    'onClick'
-  > {}
+import { useMediaQuery } from "@/common/hooks/useMediaQuery";
+import { Menu } from "./Menu";
 
-export function HeaderTitle({ children, ...rest }: IProps) {
-  const [open, setOpen] = useState(false)
-  const isMobile = useMediaQuery('(max-width: 768px)')
+type HeaderTitleType = Omit<
+  DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>,
+  "onClick"
+>;
+
+export function HeaderTitle({ children, ...rest }: Readonly<HeaderTitleType>) {
+  const [open, setOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   const handleOpenMenu = () => {
     if (isMobile) {
-      setOpen(true)
+      setOpen(true);
     }
-  }
+  };
 
   return (
     <>
@@ -29,12 +29,12 @@ export function HeaderTitle({ children, ...rest }: IProps) {
       </Wrapper>
       <Menu open={open} setOpen={setOpen} />
     </>
-  )
+  );
 }
 
 export const HeaderTitleNoSSR = dynamic(() => Promise.resolve(HeaderTitle), {
   ssr: false,
-})
+});
 
 const Wrapper = tw.h2`
 flex
@@ -44,4 +44,4 @@ dark:text-white
 text-xl
 font-bold
 cursor-pointer
-`
+`;
