6ac52b00f421e3ea05657f99a9deb30e03d028a5
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 18:21:11 +0530

    type

--- src/common/features/task/modal/ui/TaskSubtasks.tsx
+++ src/common/features/task/modal/ui/TaskSubtasks.tsx
@@ -1,40 +1,43 @@
-import { FieldArray } from "@/common/components/field-array";
-import { SubtaskType } from "@/common/types/SubtaskType";
 import { Dispatch, SetStateAction, memo } from "react";
 import { v4 as uuid } from "uuid";
 
-interface IProps {
+import { FieldArray } from "@/common/components/field-array";
+import { SubtaskType } from "@/common/types/SubtaskType";
+
+type TaskSubtasksType = {
   subtasks: SubtaskType[];
   setSubtasks: Dispatch<SetStateAction<SubtaskType[]>>;
-}
+};
 
-export const TaskSubtasks = memo(({ subtasks, setSubtasks }: IProps) => {
-  const handleAddSubtask = (value: string) => {
-    const newSubtask = {
-      id: uuid(),
-      title: value,
-      doing: false,
+export const TaskSubtasks = memo(
+  ({ subtasks, setSubtasks }: TaskSubtasksType) => {
+    const handleAddSubtask = (value: string) => {
+      const newSubtask = {
+        id: uuid(),
+        title: value,
+        doing: false,
+      };
+
+      setSubtasks([...subtasks, newSubtask]);
     };
 
-    setSubtasks([...subtasks, newSubtask]);
-  };
+    const handleRemoveSubtask = (index: number) => {
+      const newSubtasks = subtasks.filter((_, i) => i !== index);
+      setSubtasks(newSubtasks);
+    };
 
-  const handleRemoveSubtask = (index: number) => {
-    const newSubtasks = subtasks.filter((_, i) => i !== index);
-    setSubtasks(newSubtasks);
-  };
-
-  return (
-    <div className="mb-7">
-      <FieldArray
-        values={subtasks}
-        setValues={setSubtasks}
-        titleSelector="title"
-        addValue={handleAddSubtask}
-        removeValue={handleRemoveSubtask}
-      />
-    </div>
-  );
-});
+    return (
+      <div className="mb-7">
+        <FieldArray
+          values={subtasks}
+          setValues={setSubtasks}
+          titleSelector="title"
+          addValue={handleAddSubtask}
+          removeValue={handleRemoveSubtask}
+        />
+      </div>
+    );
+  },
+);
 
 TaskSubtasks.displayName = "TaskSubtasks";
