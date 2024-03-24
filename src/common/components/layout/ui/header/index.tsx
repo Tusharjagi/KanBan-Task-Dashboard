84c3ddaf8653f4261b899b5b8b3a22961ee2269b
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 17:42:58 +0530

    type

--- src/common/components/layout/ui/header/index.tsx
+++ src/common/components/layout/ui/header/index.tsx
@@ -1,38 +1,39 @@
-import tw from 'tailwind-styled-components'
-import { useLayoutContext } from '../../model/LayoutProvider'
-import { ReactNode, useEffect, useState } from 'react'
-import { Menu } from './Menu'
-import { useMediaQuery } from '@/common/hooks/useMediaQuery'
-import { bool2string, string2bool } from '@/common/utils'
+import { ReactNode, useEffect, useState } from "react";
+import tw from "tailwind-styled-components";
 
-interface IProps {
-  children?: ReactNode
-}
+import { useLayoutContext } from "../../model/LayoutProvider";
+import { Menu } from "./Menu";
+import { useMediaQuery } from "@/common/hooks/useMediaQuery";
+import { bool2string, string2bool } from "@/common/utils";
 
-export function Header({ children }: IProps) {
-  const { expanded } = useLayoutContext()
+type HeaderType = {
+  children?: ReactNode;
+};
 
-  const [open, setOpen] = useState(false)
-  const [mounted, setMounted] = useState(false)
-  const isDesktop = useMediaQuery('(min-width: 768px)')
+export function Header({ children }: Readonly<HeaderType>) {
+  const { expanded } = useLayoutContext();
+
+  const [open, setOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
   return (
     <Wrapper $expanded={bool2string(expanded && mounted && isDesktop)}>
       {children}
       <Menu open={open} setOpen={setOpen} />
     </Wrapper>
-  )
+  );
 }
 
-interface IWrapperProps {
-  $expanded: 'true' | 'false'
-}
+type WrapperType = {
+  $expanded: "true" | "false";
+};
 
-const Wrapper = tw.header<IWrapperProps>`
+const Wrapper = tw.header<WrapperType>`
 flex
 items-center
 justify-between
@@ -46,6 +47,6 @@ p-5
 md:h-20
 z-50
 bg-white
-dark:bg-[#2b2c37]
-${({ $expanded }) => (string2bool($expanded) ? 'left-[256px]' : 'left-0')}
-`
+dark:bg-gunmetal
+${({ $expanded }) => (string2bool($expanded) ? "left-[256px]" : "left-0")}
+`;
