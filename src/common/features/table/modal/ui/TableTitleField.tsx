a29d99336a038dd2d52e37172afa115a969a3500
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 18:17:44 +0530

    type

--- src/common/features/table/modal/ui/TableTitleField.tsx
+++ src/common/features/table/modal/ui/TableTitleField.tsx
@@ -1,28 +1,31 @@
-import { Input, InputLabel, InputField } from '@/common/ui/input'
-import { ChangeEvent, Dispatch, SetStateAction, memo } from 'react'
+import { ChangeEvent, Dispatch, SetStateAction, memo } from "react";
 
-interface IProps {
-  title: string
-  setTitle: Dispatch<SetStateAction<string>>
-}
+import { Input, InputLabel, InputField } from "@/common/ui/input";
 
-export const TableTitleField = memo(({ title, setTitle }: IProps) => {
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value)
-  }
+type TableTitleFieldType = {
+  title: string;
+  setTitle: Dispatch<SetStateAction<string>>;
+};
 
-  return (
-    <Input>
-      <InputLabel>Board name</InputLabel>
-      <InputField
-        id="board-name"
-        type="text"
-        className="mb-5"
-        value={title}
-        onChange={handleOnChange}
-      />
-    </Input>
-  )
-})
+export const TableTitleField = memo(
+  ({ title, setTitle }: TableTitleFieldType) => {
+    const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setTitle(e.target.value);
+    };
 
-TableTitleField.displayName = 'TableTitleField'
+    return (
+      <Input>
+        <InputLabel>Board name</InputLabel>
+        <InputField
+          id="board-name"
+          type="text"
+          className="mb-5"
+          value={title}
+          onChange={handleOnChange}
+        />
+      </Input>
+    );
+  },
+);
+
+TableTitleField.displayName = "TableTitleField";
