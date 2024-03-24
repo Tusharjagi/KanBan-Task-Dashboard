46cc2029fab13364185bcd5b667ed5bcbb441401
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 18:55:50 +0530

    type

--- src/common/ui/switcher/index.tsx
+++ src/common/ui/switcher/index.tsx
@@ -1,30 +1,30 @@
-import tw from 'tailwind-styled-components'
+import tw from "tailwind-styled-components";
 
-interface IProps {
-  value: boolean
-  onChange: (value: boolean) => void
-}
+type SwitcherType = {
+  value: boolean;
+  onChange: (value: boolean) => void;
+};
 
-export function Switcher({ value, onChange }: IProps) {
+export function Switcher({ value, onChange }: Readonly<SwitcherType>) {
   const handleOnClick = () => {
-    onChange(!value)
-  }
+    onChange(!value);
+  };
 
   return (
     <Wrapper
-      className={value ? 'bg-indigo-500' : 'bg-indigo-100'}
+      className={value ? "bg-indigo-500" : "bg-indigo-100"}
       onClick={handleOnClick}
     >
       <Input value={`${value}`} type="checkbox" />
       <Circle
         className={
           value
-            ? 'bg-white translate-x-[100%]'
-            : 'translate-x-[0%] bg-slate-400'
+            ? "bg-white translate-x-[100%]"
+            : "translate-x-[0%] bg-slate-400"
         }
       />
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = tw.div`
@@ -33,13 +33,13 @@ w-10
 h-5
 rounded-full
 cursor-pointer
-`
+`;
 
 const Input = tw.input`
 hidden
 w-0
 h-0
-`
+`;
 
 const Circle = tw.span`
 absolute
@@ -51,4 +51,4 @@ h-[16px]
 rounded-full
 pointer-events-none
 transition-transform
-`
+`;
