b120b3202e32132fec4dc78d5e5bdc6c1ea9947e
Tusharjagi <tusharjagi@gmail.com>
2024-03-24 13:34:10 +0530

    empty board name handle error

--- src/common/features/table/modal/CreateTableModal.tsx
+++ src/common/features/table/modal/CreateTableModal.tsx
@@ -1,50 +1,65 @@
-import { Modal, ModalTitle } from '@/common/ui/modal'
-import tw from 'tailwind-styled-components'
-import { Button } from '@/common/ui/button'
-import { FormEvent, useRef, useState } from 'react'
-import { DEFAULT_COLUMNS_VALUES } from '@/common/data/constants'
-import { useAppDispatch } from '@/common/hooks/useRedux'
-import { mainActions } from '@/common/store/slices/main'
-import { ITableColumn } from '@/common/models/ITableColumn'
-import { TableTitleField } from './ui/TableTitleField'
-import { TableColumns } from './ui/TableColumns'
-import { v4 as uuid } from 'uuid'
+import { FormEvent, useRef, useState } from "react";
+import { useAppDispatch } from "@/common/hooks/useRedux";
+
+import { Modal, ModalTitle } from "@/common/ui/modal";
+import tw from "tailwind-styled-components";
+import { Button } from "@/common/ui/button";
+import { DEFAULT_COLUMNS_VALUES } from "@/common/data/constants";
+import { mainActions } from "@/common/store/slices/main";
+import { ITableColumn } from "@/common/models/ITableColumn";
+import { TableTitleField } from "./ui/TableTitleField";
+import { TableColumns } from "./ui/TableColumns";
+import { v4 as uuid } from "uuid";
 
 interface IProps {
-  open: boolean
-  onClose: VoidFunction
+  open: boolean;
+  onClose: VoidFunction;
 }
 
-export function CreateTableModal(props: IProps) {
-  const ref = useRef<HTMLFormElement>(null)
+export function CreateTableModal(props: Readonly<IProps>) {
+  const ref = useRef<HTMLFormElement>(null);
 
-  const [title, setTitle] = useState('')
-  const [columns, setColumns] = useState<ITableColumn[]>(DEFAULT_COLUMNS_VALUES)
+  const [title, setTitle] = useState("");
+  const [columns, setColumns] = useState<ITableColumn[]>(DEFAULT_COLUMNS_VALUES);
+  const [showError, setShowError] = useState(false);
 
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-  const { onClose: handleClose } = props
+  const { onClose: handleClose } = props;
 
   const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    dispatch(
-      mainActions.addTable({
-        id: uuid(),
-        title,
-        columns,
-      })
-    )
+    if (title.length > 0) {
+      dispatch(
+        mainActions.addTable({
+          id: uuid(),
+          title,
+          columns,
+        }),
+      );
+      ref.current!.reset();
+      setTitle("");
+      handleClose();
+    } else {
+      setShowError(true);
+    }
+  };
 
-    ref.current!.reset()
-
-    handleClose()
-  }
+  const handleOnChange = (event: FormEvent<HTMLFormElement>) => {
+    const target = event.target as HTMLInputElement;
+    target.value.length > 0 && setShowError(false);
+  };
 
   return (
     <Modal {...props}>
-      <form ref={ref} onSubmit={handleOnSubmit}>
+      <form ref={ref} onSubmit={handleOnSubmit} onChange={handleOnChange}>
         <ModalTitle className="mb-7">Add new table</ModalTitle>
+        {showError && (
+          <div className="text-white flex justify-center items-center text-sm my-1 mx-10 bg-red-600 rounded-2xl">
+            {`Board Name shouldn't be empty`}
+          </div>
+        )}
         <TableTitleField title={title} setTitle={setTitle} />
         <ColumnsTitle>Table columns</ColumnsTitle>
         <TableColumns columns={columns} setColumns={setColumns} />
@@ -53,7 +68,7 @@ export function CreateTableModal(props: IProps) {
         </Button>
       </form>
     </Modal>
-  )
+  );
 }
 
 const ColumnsTitle = tw.div`
@@ -61,4 +76,4 @@ text-white
 text-base
 font-bold
 mb-2
-`
+`;
